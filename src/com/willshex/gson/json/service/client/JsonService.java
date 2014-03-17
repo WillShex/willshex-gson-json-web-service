0ed89a3f8439d1bf511166b36a8c04729b904396
William Shakour <william.shakour@gmail.com>
2014-03-17 20:08:03 +0000

    if an event bus is assigned to a json service it will receive events on call start, success and failure

--- src/com/willshex/gson/json/service/client/JsonService.java
+++ src/com/willshex/gson/json/service/client/JsonService.java
@@ -1,10 +1,14 @@
 package com.willshex.gson.json.service.client;
 
+import com.google.gwt.event.shared.HasHandlers;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.URL;
 import com.spacehopperstudios.utility.JsonUtils;
+import com.willshex.gson.json.service.client.JsonServiceCallEventHandler.JsonServiceCallFailure;
+import com.willshex.gson.json.service.client.JsonServiceCallEventHandler.JsonServiceCallStart;
+import com.willshex.gson.json.service.client.JsonServiceCallEventHandler.JsonServiceCallSuccess;
 import com.willshex.gson.json.service.shared.Error;
 import com.willshex.gson.json.service.shared.Request;
 import com.willshex.gson.json.service.shared.Response;
@@ -12,14 +16,19 @@ import com.willshex.gson.json.service.shared.StatusType;
 
 public abstract class JsonService {
 
-	protected String Url;
+	protected String url;
+	protected HasHandlers bus;
 
 	public String getUrl() {
-		return Url;
+		return url;
 	}
 
-	public void setUrl(String url) {
-		Url = url;
+	public void setUrl(String value) {
+		url = value;
+	}
+
+	public void setBus(HasHandlers value) {
+		bus = value;
 	}
 
 	protected void parseResponse(String responseText, Response outputParameter) {
@@ -33,16 +42,34 @@ public abstract class JsonService {
 		}
 	}
 
-	protected void sendRequest(String action, Request input, RequestCallback callback) throws RequestException {
+	protected com.google.gwt.http.client.Request sendRequest(String action, Request input, RequestCallback callback) throws RequestException {
 		String requestData = "action=" + action + "&request=";
 
-		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, URL.encode(Url));
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, URL.encode(url));
 
 		requestData += URL.encodeQueryString(JsonUtils.cleanJson(input.toJson().toString()));
 
 		builder.setHeader("Content-Type", "application/x-www-form-urlencoded");
 
-		builder.sendRequest(requestData, callback);
+		return builder.sendRequest(requestData, callback);
+	}
+
+	protected void onCallStart(JsonService origin, String callName, Request input, com.google.gwt.http.client.Request requestHandle) {
+		if (bus != null) {
+			bus.fireEvent(new JsonServiceCallStart(origin, callName, input, requestHandle));
+		}
+	}
+
+	protected void onCallSuccess(JsonService origin, String callName, Request input, Response output) {
+		if (bus != null) {
+			bus.fireEvent(new JsonServiceCallSuccess(origin, callName, input, output));
+		}
+	}
+
+	protected void onCallFailure(JsonService origin, String callName, Request input, Throwable caught) {
+		if (bus != null) {
+			bus.fireEvent(new JsonServiceCallFailure(origin, callName, input, caught));
+		}
 	}
 
 }
